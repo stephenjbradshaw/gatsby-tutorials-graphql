5927df98207be6fac99d2e6d54a776fb810af38a
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-10 15:01:10 +0100

    Add slugs to nodes using createNodeField

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.onCreateNode = ({ node, getNode }) => {
-  if (node.internal.type === `MarkdownRemark`) {
-    console.log(createFilePath({node, getNode, basePath: `pages`}))
+// Data in gatsby is organized as nodes in a graph
+
+/* 
+  When a node is created by the remark transformer plugin (i.e. as a page is created from markdown):
+  Get the parent (file) node and make a slug out of it using createFilePath()
+  ```
+  exports.onCreateNode = ({ node, getNode }) => {
+    if (node.internal.type === `MarkdownRemark`) {
+      console.log(createFilePath({node, getNode, basePath: `pages`}))
+    }
+  }  
+  ```
+*/
+
+/*
+ Generate a slug as above
+ Then edit the node you got it from to add a new field with the slug (createNodeField)
+ This value can then be read with graphql!! (see next step)
+ */
+
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({ node, getNode, basePath: "pages" })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
   }
 }
